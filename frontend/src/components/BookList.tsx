a1f0e8ab6f30dd350757478f343942c350b6cda0
Rehema Mwaka <mwakabayah@gmail.com>
2024-06-08 19:45:37 +0300

    Display the books in a grid

--- frontend/src/components/BookList.tsx
+++ frontend/src/components/BookList.tsx
@@ -8,9 +8,11 @@ import {
   Card,
   CardContent,
   CardMedia,
+  CardActions,
+  Grid,
 } from "@mui/material";
 import { useReadingList } from "../context/ReadingListContext";
-
+import ReadingList from "./ReadingList";
 interface BookListProps {
   searchTerm: string;
 }
@@ -34,32 +36,46 @@ const BookList: React.FC<BookListProps> = ({ searchTerm }) => {
   };
 
   return (
-    <Box>
-      {filteredBooks.map((book: any) => (
-        <Card key={book.title} sx={{ display: "flex", mb: 2 }}>
-          <CardMedia
-            component="img"
-            sx={{ width: 151 }}
-            // image={require("../assets/image1.webp")}
-            image={require(`../${book.coverPhotoURL}`)}
-            alt={`Cover of ${book.title}`}
-            onError={() => handleImageError(book.title)}
-          />
-          <Box sx={{ display: "flex", flexDirection: "column" }}>
-            <CardContent>
-              <Typography variant="h6">{book.title}</Typography>
-              <Typography variant="subtitle1">{book.author}</Typography>
-              <Button
-                variant="contained"
-                onClick={() => addToReadingList(book)}
+    <Grid container spacing={3}>
+      <Grid item xs={8}>
+        <Grid container spacing={3}>
+          {filteredBooks.map((book: any) => (
+            <Grid item xs={12} sm={6} md={4} key={book.title}>
+              <Card
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  height: "100%",
+                }}
               >
-                Add to Reading List
-              </Button>
-            </CardContent>
-          </Box>
-        </Card>
-      ))}
-    </Box>
+                <CardMedia
+                  component="img"
+                  sx={{ height: 250 }}
+                  image={require(`../${book.coverPhotoURL}`)}
+                  alt={`Cover of ${book.title}`}
+                  onError={() => handleImageError(book.title)}
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography variant="subtitle1">{book.title}</Typography>
+                  <Typography variant="body2">{`by ${book.author}`}</Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    variant="contained"
+                    onClick={() => addToReadingList(book)}
+                  >
+                    Add to Reading List
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+      <Grid item xs={4}>
+        <ReadingList />
+      </Grid>
+    </Grid>
   );
 };
 
--- frontend/src/components/ReadingList.tsx
+++ frontend/src/components/ReadingList.tsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { Box, Button, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Typography,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+} from "@mui/material";
 import { useReadingList } from "../context/ReadingListContext";
 
 const ReadingList: React.FC = () => {
@@ -7,19 +15,33 @@ const ReadingList: React.FC = () => {
 
   return (
     <Box>
-      <Typography variant="h5">Reading List</Typography>
-      {readingList.map((book) => (
-        <Box key={book.title} mb={2}>
-          <Typography variant="h6">{book.title}</Typography>
-          <Typography variant="subtitle1">{book.author}</Typography>
-          <Button
-            variant="contained"
-            onClick={() => removeFromReadingList(book.title)}
-          >
-            Remove
-          </Button>
-        </Box>
-      ))}
+      <Typography variant="h5" mb={2}>
+        Reading List
+      </Typography>
+      {readingList.length === 0 ? (
+        <Typography>Your reading list will appear here.</Typography>
+      ) : (
+        readingList.map((book) => (
+          <Card key={book.title} sx={{ display: "flex", mb: 2 }}>
+            <CardMedia
+              component="img"
+              sx={{ width: 50, height: 100 }}
+              image={require(`../${book.coverPhotoURL}`)}
+              alt={`Cover of ${book.title}`}
+            />
+            <CardContent sx={{ display: "flex", flexDirection: "column" }}>
+              <Typography variant="subtitle2">{book.title}</Typography>
+              <Typography variant="body2">{`by ${book.author}`}</Typography>
+              <Button
+                variant="contained"
+                onClick={() => removeFromReadingList(book.title)}
+              >
+                Remove
+              </Button>
+            </CardContent>
+          </Card>
+        ))
+      )}
     </Box>
   );
 };
